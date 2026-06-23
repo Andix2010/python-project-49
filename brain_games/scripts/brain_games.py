3865de0b01c1e158c00ae1afaccaf2993648cbbb
Andix <Itaci2009@yandex.ru>
2026-06-23 16:14:12 +0400

    add SonarQube and ruff

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
-from brain_games.greeting import greet
 from brain_games.cli import welcome_user
+from brain_games.greeting import greet
+
+
 def main():
     greet()
     welcome_user()
+
 
 if __name__ == "__main__":
     main()
