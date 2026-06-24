d071008dc8084f86015a2d92ba9cff2189676874
Andix <Itaci2009@yandex.ru>
2026-06-24 20:02:12 +0400

    update even game for new game logic with engine

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,22 +1,14 @@
-import prompt
 import random
 
+from brain_games.general_engine import run_game
 
-def run_even_game(name):
-    right_answer_count = 0
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    while right_answer_count < 3:
-        number = random.randint(1, 99)
-        print(f"Question: {number}")
-        answer = prompt.string('Your answer: ')
-        right_answer = 'yes' if (number % 2 == 0) else 'no'
-        if answer == right_answer:
-            print("Correct!")
-            right_answer_count += 1
-        else:
-            print(f"'{answer}' is wrong answer ;(. "
-                f"Correct answer was '{right_answer}'.\n"
-                f"Let's try again, {name}!")
-            return 
-    print(f"Congratulations, {name}!")
-    return
+
+def run_even_game(name: str) -> None:
+    run_game(get_even_round, name, 
+            'Answer "yes" if the number is even, otherwise answer "no".')
+        
+
+def get_even_round():
+    number = random.randint(1, 99)
+    right_answer = 'yes' if (number % 2 == 0) else 'no'
+    return (str(number), right_answer)
